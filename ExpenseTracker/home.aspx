e9e1e0ad9b2075725c20ac62594da410b37c6788
hlrodriguez85 <hlrodriguez_85@msn.comgit config --global user.email hlrodriguez_85@msn.com>
2020-01-27 14:24:08 -0600

    Acklen Dev Challenge

---
+++ ExpenseTracker/home.aspx
@@ -0,0 +1,74 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="home.aspx.cs" Inherits="ExpenseTracker.home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+     <script type="text/javascript">
+         //Function to allow only numbers to textbox
+         function isNumberKey(key) {
+             var keycode = (key.which) ? key.which : key.keyCode;
+             var phn = document.getElementById('txtPhn');
+             if (!(keycode == 8 || keycode == 46) && (keycode < 48 || keycode > 57)) {
+                 return false;
+             }
+             else {
+                 if (phn.value.length < 10) {
+                     return true;
+                 }
+                 else {
+                     return false;
+                 }
+             }
+         }
+</script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>EXPENSE TRACKER</h1>
+            <table>
+                <tr><td>Purchase: </td></tr>
+                <tr><td>
+                     <asp:TextBox ID="PurchaseTXT" MaxLength="50" runat="server"/>
+                </td></tr>
+                <tr><td>Category: </td></tr>
+                <tr><td><asp:DropDownList ID="CategoryDDL" runat="server">
+                    <asp:ListItem Text="Groceries" />
+                    <asp:ListItem Text="Public Services" />
+                    <asp:ListItem Text="Housing" />
+                    <asp:ListItem Text="Shopping" />
+                    <asp:ListItem Text="Personal" />
+                    <asp:ListItem Text="Other" />
+                    </asp:DropDownList></td></tr>
+                <tr><td>Amount: </td></tr>
+                <tr><td><asp:TextBox ID="AmountTRX" MaxLength="50" runat="server" onkeypress="return isNumberKey(event)"/>
+                </td></tr>
+                <tr><td>
+                    <asp:Button ID="AddTRX" runat="server" Text ="Add Purchase" OnClick="AddTRX_Click"/>
+                </td></tr>
+            </table>
+        </div>
+        <div>
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </div>
+        <div>
+            <table>
+                <tr><td>
+                    <h3>Total spent this month: </h3>
+                </td><td>
+                    <asp:Label ID="TotalLbl" runat="server" Font-Bold="True" ForeColor="Red"></asp:Label></td><td>
+                </td></tr>
+                <tr><td>
+                    <h3>Highest spending category: </h3>
+                </td><td>
+                    <asp:Label ID="HighestCat" runat="server" Font-Bold="True" ForeColor="Red"></asp:Label>
+                    <asp:Label ID="HighAmount" runat="server" Font-Bold="True" ForeColor="Red"></asp:Label>
+                </td></tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ExpenseTracker/home.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Data.Sql;
+
+namespace ExpenseTracker
+{
+    public partial class home : System.Web.UI.Page
+    {
+        string conn = "Data Source=DROGON;Initial Catalog=Test;Integrated Security=True";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ShowGrid();
+            LabelText();
+            HighCat();
+        }
+
+        protected void AddTRX_Click(object sender, EventArgs e)
+        {
+            if (PurchaseTXT.Text == "" || AmountTRX.Text == "")
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "Alert", "alert('Please fill all the spaces.');", true);
+            }
+            else
+            {
+                try
+                {
+                    SqlConnection con = new SqlConnection(conn);
+                    con.Open();
+
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.Connection = con;
+                        cmd.CommandType = CommandType.Text;
+                        cmd.CommandText = "insert into TRX (Date_TRX, Amount, Category, Purchase) values (cast(GETDATE() as date), @Amount, @Category, @Purchase)";
+                        cmd.Parameters.AddWithValue("@Amount", AmountTRX.Text);
+                        cmd.Parameters.AddWithValue("@Category", CategoryDDL.Text);
+                        cmd.Parameters.AddWithValue("@Purchase", PurchaseTXT.Text);
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected == 1)
+                        {
+                            ClientScript.RegisterStartupScript(this.GetType(), "Succes", "alert('Purchase submitted.');", true);
+                            PurchaseTXT.Text = "";
+                            AmountTRX.Text = "";
+                            ShowGrid();
+                            LabelText();
+                            HighCat();
+                        }
+                        else
+                        {
+                            ClientScript.RegisterStartupScript(this.GetType(), "Alert", "alert('Something went wrong. Please try again');", true);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error adding date: '{ex}'");
+                    ClientScript.RegisterStartupScript(this.GetType(), "Alert", "alert('Something went wrong. Error: {ex}');", true);
+                }
+            }
+        }
+
+        public void ShowGrid ()
+        {
+            SqlConnection con = new SqlConnection(conn);
+            string com = "select Date_TRX as 'Date', Purchase, Amount, Category from TRX";
+            SqlDataAdapter adpt = new SqlDataAdapter(com, con);
+            DataTable dt = new DataTable();
+            adpt.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+        public void LabelText()
+        {
+            SqlConnection con = new SqlConnection(conn);
+            string com = "select '$'+cast(sum(amount) as varchar) Total from TRX where DATEPART(month, Date_TRX) = DATEPART(month, getdate()) and DATEPART(year, Date_TRX) = DATEPART(year, getdate())";
+            SqlDataAdapter adpt = new SqlDataAdapter(com, con);
+            DataTable dt = new DataTable();
+            adpt.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                //TotalLbl.Text = dt.Rows[0]("Total").ToString();
+                TotalLbl.Text = dt.Rows[0][0].ToString();
+            }
+        }
+
+        public void HighCat()
+        {
+            SqlConnection con = new SqlConnection(conn);
+            string com = "select Category, Sum(amount) Total into #TEMP from TRX group by Category select Category, ' $'+cast(Total as varchar) from #TEMP order by Total DESC drop table #TEMP";
+            SqlDataAdapter adpt = new SqlDataAdapter(com, con);
+            DataTable dt = new DataTable();
+            adpt.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                //TotalLbl.Text = dt.Rows[0]("Total").ToString();
+                HighestCat.Text = dt.Rows[0][0].ToString();
+                HighAmount.Text = dt.Rows[0][1].ToString();
+            }
+        }
+    }
+}
